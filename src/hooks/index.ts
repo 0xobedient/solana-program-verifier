1116a0dc65b370bc6dd2cff4c6f1fbc3218e1b77
resister-boy <pesticide.dev@gmail.com>
2025-07-09 19:30:08 +0900

    feat: udpate unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,4 +3,3 @@ export * from "./get_program_verification_status";
 export * from "./get_verification_job_status";
 export * from "./get_verified_program";
 export * from "./verify_program";
-export * from "./verify_program_sync";
--- src/shared/constants/endpoint.ts
+++ src/shared/constants/endpoint.ts
@@ -2,7 +2,6 @@ export const OSEC_ENDPOINT_URI: string = "https://verify.osec.io";
 
 export const OSEC_ROUTER = {
   VERIFY_PROGRAM: "/verify",
-  VERIFY_SYNC_PROGRAM: "/verify/sync",
   GET_PROGRAM_BUILD_LOG: "/logs/",
   GET_PROGRAM_VERIFICATION_STATUS: "/status/",
   GET_VERIFIED_PROGRAM_STATUS: "/verified-program/status",
